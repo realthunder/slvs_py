61400d81bbdd49a9b1782d036870c92ffe86c058
Zheng, Lei <realthunder.dev@gmail.com>
2020-12-02 11:21:25 +0800

    Try to bypass ninjia dupbuild error

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from skbuild import setup
 
-from os import path
-from io import open
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+import os, io
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 if __name__ == '__main__':
+
+    # hackrish way to bypass ninjia dupbuild error
+    os.environ['SKBUILD_BUILD_OPTIONS'] = '-w dupbuild=warn'
+
     setup(
         name='py_slvs',
         version='1.0.0',
@@ -14,7 +17,7 @@ if __name__ == '__main__':
         license='Gnu General Public License 3.0',
         author='Zheng, Lei',
         author_email='realthunder.dev@gmail.com',
-        cmake_args=['-DENABLE_GUI:BOOL=OFF','-DBUILD_PYTHON:BOOL=ON','--', '-w', 'dupbuild=warn'],
+        cmake_args=['-DENABLE_GUI:BOOL=OFF','-DBUILD_PYTHON:BOOL=ON'],
         cmake_source_dir='slvs',
         url='https://github.com/realthunder/slvs_py',
         description='Python binding of SOLVESPACE geometry constraint solver',
